cdf351c45e5d870768cb8e09cd415230ca63e6aa
Rofael Aleezada <me@rofael.com>
2019-01-15 14:18:24 -0600

    Part 1 of HW2

--- hw2/hw2.m
+++ hw2/hw2.m
@@ -1,9 +1,16 @@
 raw = imread('raw.tiff');
 
+% Write data about tiff to file
 handle = fopen('hw1_473sp19.txt','w');
-fprintf(handle, '%d\n', size(raw, 1));
-fprintf(handle, '%d\n', size(raw, 2));
-fprintf(handle, '%s\n', class(raw(1,1)));
-fprintf(handle, '%d\n', min(raw));
-fprintf(handle, '%d\n', max(raw));
+fprintf(handle, 'width: %d\n', size(raw, 2));
+fprintf(handle, 'height: %d\n', size(raw, 1));
+fprintf(handle, 'type: %s\n', class(raw(1,1)));
+fprintf(handle, 'minimum pixel: %d\n', min(raw(:)));
+fprintf(handle, 'maximum pixel: %d\n', max(raw(:)));
 fclose(handle);
+
+% Display raw image, then cast to double
+title('raw image');
+imshow(raw);
+raw = double(raw);
+
