d0928e9011f6fff73cce3ffcec1f54693c3f6537
Rofael Aleezada <me@rofael.com>
2019-01-28 12:58:26 -0600

    Finished HW2

--- hw2/hw2.m
+++ hw2/hw2.m
@@ -9,12 +9,11 @@ fprintf(handle, 'minimum pixel: %d\n', min(raw(:)));
 fprintf(handle, 'maximum pixel: %d\n', max(raw(:)));
 
 % Display raw image, then cast to double
-% imshow(raw);
-% title('raw image');
+imshow(raw);
+title('raw image');
 raw = double(raw);
-
 fprintf('Program paused. Press enter to continue.\n');
-% pause;
+   pause;
 
 % linearization
 black_level = 2047;
@@ -26,24 +25,22 @@ im_linear(im_linear >= sat_level) = 1;
 im_linear(im_linear > 1) = ...
     im_linear(im_linear > 1) / (black_level + sat_level);
 
-% imshow(im_linear); 
-% title('linearized image');
+imshow(im_linear); 
+title('linearized image');
 imwrite(im_linear, 'linearized.jpg');
-
 fprintf('Program paused. Press enter to continue.\n');
-% pause;
+pause;
 
 % bayer mosaic
 im_zoom = im_linear(1000:1099, 901:1000);
 im_zoom = im_zoom * 5;
 im_zoom(im_zoom > 1) = 1;
 
-% imshow(im_zoom, 'InitialMagnification', 500);
-% title('bayer mosaic');
+imshow(im_zoom, 'InitialMagnification', 500);
+title('bayer mosaic');
 imwrite(im_zoom, 'zoomed.jpg')
-
 fprintf('Program paused. Press enter to continue.\n');
-% pause;
+pause;
 
 % extract bayer data
 top_left = im_linear(1:2:end, 1:2:end);  % blue
@@ -58,16 +55,55 @@ im_gbrg = 4 * cat(3, bot_left, (top_left + bot_right) / 2, top_right);
 
 subplot(2,2,1);
 imshow(im_bggr);
+title('BGGR');
 subplot(2,2,2);
 imshow(im_rggb);
+title('RGGB');
 subplot(2,2,3);
 imshow(im_grbg);
+title('GRBG');
 subplot(2,2,4);
 imshow(im_gbrg);
+title('GBRG');
+fprintf('Program paused. Press enter to continue.\n');
+pause;
 
-imwrite(im_rggb, 'best_bayer.jpg')
-fprintf(handle, 'best bayer mosaic: rggb\n');
+imwrite(im_bggr, 'best_bayer.jpg');
+imshow(im_bggr);
+title('best mosaic - BGGR');
+fprintf(handle, 'best bayer mosaic: bggr\n');
 fclose(handle);
 
 fprintf('Program paused. Press enter to continue.\n');
-% pause;
+pause;
+
+% white balancing
+red_channel = top_left;
+green_channel = (top_right + bot_left) / 2;
+blue_channel = bot_right;
+
+red_avg = mean(red_channel, 'all');
+red_max = max(red_channel(:));
+green_avg = mean(green_channel, 'all');
+green_max = max(green_channel(:));
+blue_avg = mean(blue_channel, 'all');
+blue_max = max(blue_channel(:));
+
+grey_world_red_channel = red_channel * (green_avg / red_avg);
+grey_world_blue_channel = blue_channel * (green_avg / blue_avg);
+grey_world = 4 * cat(3, grey_world_red_channel, green_channel, ...
+    grey_world_blue_channel);
+imwrite(grey_world, 'grey_world.jpg');
+
+white_world_red_channel = red_channel * (green_max / red_max);
+white_world_blue_channel = blue_channel * (green_max / blue_max);
+white_world = 4 * cat(3, white_world_red_channel, green_channel, ...
+    white_world_blue_channel);
+imwrite(white_world, 'white_world.jpg');
+
+subplot(2,1,1);
+imshow(grey_world);
+title('grey world');
+subplot(2,1,2);
+imshow(white_world);
+title('white world');
