9a63e6410a4f447a0cea27b5cd23e2a95ea5a976
Rofael Aleezada <me@rofael.com>
2019-02-08 10:16:28 -0600

    HW3 optimizations

--- hw3/hybrid_image.m
+++ hw3/hybrid_image.m
@@ -4,25 +4,31 @@ function [im1_lo, im2_hi, im_hybrid] = hybrid_image (im1, im2, G1, G2)
 % in a 3x2 grid, display the 2 original images (first row),
 % low-pass-filtered and high-pass filtered images (second row),
 % and hybrid image (3rd row)
-
 filter = fspecial('gaussian', [31 31], G1);
 im1_lo = imfilter(im1, filter);
 
-filter = fspecial('gaussian', [31 31], G2);
-im2_hi = im2 - imfilter(im2, filter);
+if G1 ~= G2
+    % Compute other Gaussian filter if the G values aren't equal
+    filter = fspecial('gaussian', [31 31], G2);
+end
 
+im2_hi = im2 - imfilter(im2, filter);
 im_hybrid = im1_lo + im2_hi;
 
 subplot(3,2,1);
 imshow(im1);
+
 subplot(3,2,2);
 imshow(im2);
+
 subplot(3,2,3);
 imshow(im1_lo);
 title('Low Pass');
+
 subplot(3,2,4);
 imshow(im2_hi + 0.5);
 title('High Pass (Brightened)');
+
 subplot(3,2,5);
 imshow(im_hybrid);
 title('Hybrid');
--- hw3/viz_freq.m
+++ hw3/viz_freq.m
@@ -1,11 +1,17 @@
 function viz_freq (img_lo, img_hi)
+
+% precompute the gaussian filters
+filters = zeros(31, 31, 9);
+for sig = 1:9
+    filters(:, :, sig) = fspecial('gaussian', [31 31], sig);
+end
+
 % display a 3x3 grid of the first image at progressively larger sigma
 % which can be used to choose sigma
 figure;
 for sig = 1:9
     % low pass - blurring 
-    filter = fspecial('gaussian', [31 31], sig);
-    img = imfilter(img_lo, filter);
+    img = imfilter(img_lo, filters(:, :, sig));
     
     subplot(3,3,sig)
     imshow(img)
@@ -17,8 +23,7 @@ end
 figure;
 for sig = 1:9
     % high pass - subtract low pass from image
-    filter = fspecial('gaussian', [31 31], sig);
-    low_pass = imfilter(img_hi, filter);
+    low_pass = imfilter(img_hi, filters(:, :, sig));
     img = (img_hi - low_pass) + 0.5;
     
     subplot(3,3,sig)
